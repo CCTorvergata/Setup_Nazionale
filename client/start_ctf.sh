2e57d6e7b3b2493916744f86f72b7430284a6161
root <giuliano.vallone@ikmail.com>
2024-06-20 18:38:44 +0200

    Ora viene aggiunto l'ip del gameserver a /etc/hosts

--- client/start_ctf.sh
+++ client/start_ctf.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-#docker network create --driver=bridge --subnet=172.18.0.0/24 tools_network
-mkdir -p /srv/services
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./$1 <vm_ip>"
+    exit 1
+fi
+
+echo "$1\tgameserver" >> /etc/hosts
+
+#Tools Setup
+docker network create --driver=bridge --subnet=172.18.0.0/24 tools_network
 docker start $(docker ps -a -q)
-ssh -l root -i /root/private_key_ctf $GAMESERVER
+
+#Save SSH fingerprint
+ssh -l root gameserver
+
+#Clone traffic locally
 sudo systemctl start get_traffic
