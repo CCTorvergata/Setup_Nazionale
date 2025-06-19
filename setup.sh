476a3aefdc612b47a671848daa3a1ea258a663e6
Giuliano Vallone <giuliano.vallone@ikmail.com>
2025-06-19 16:24:00 +0200

    Aggiornato

--- setup.sh
+++ setup.sh
@@ -2,4 +2,7 @@
 cd ansible
 
 #Esecuzione del playbook Ansible per il "client"
-ansible-playbook setup_client.yml
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+ansible-playbook -i inventory playbook.yml -K
