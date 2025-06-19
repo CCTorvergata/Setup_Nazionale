86215b2f5d985fc2b533b122c13e17ab98e7b7ce
Giuliano Vallone <giuliano.vallone@ikmail.com>
2025-06-19 18:09:17 +0200

    Now asks for ssh password

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ cd ansible
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-ansible-playbook -i inventory playbook.yml -K
+ansible-playbook -i inventory playbook.yml -K -k
