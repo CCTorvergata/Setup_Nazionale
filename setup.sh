509cc1bd8457de2ffb1718203202399435ce7e92
morels <clod.is.here@gmail.com>
2026-04-11 21:15:14 +0200

    Edited setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-#Esecuzione del playbook Ansible per il "client"
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
-ansible-playbook -i inventory site.yml -K -k
+grep -q "#VULNBOX_IP" inventory && { \
+	echo "ERROR: You forgot to set the vulnbox's IP in the file './inventory'"; \
+	exit 1; \
+}
+
+ls ./roles/vulnbox/files/public-keys/*.pub 2>&1 > /dev/null || { \
+	echo "ERROR: you need to put your SSH public key in the directory './roles/vulnbox/files/public-keys/'"; \
+	exit 1; \
+}
+
+ansible-playbook site.yml
