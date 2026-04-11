64a43d833cb30af43b70a27a15163f4ff26e299e
morels <clod.is.here@gmail.com>
2026-04-11 16:44:49 +0200

    Fixed sniffer prefix

--- roles/vulnbox/files/sniffer.sh
+++ roles/vulnbox/files/sniffer.sh
@@ -2,9 +2,9 @@
 
 # Script per lo sniffing del traffico e creazione dei file pcap
 
-ROTATE_SECONDS=15                 # Write a new pcap every tot seconds
-PREFIX=TV-                        # Prefix to add to name files (optional)
-DIRECTORY=/srv/traffic            # Directory where to save the pcap files
+ROTATE_SECONDS=15                         # Write a new pcap every tot seconds
+PREFIX="TV_$(date +%Y-%m-%d_%H.%M.%S)"    # Static prefix added to name files (optional, tshark will still suffix "_fileindex_timestamp")
+DIRECTORY=/srv/traffic                    # Directory where to save the pcap files
 INTERFACE=game
 
 if [ "$EUID" -ne 0 ]
@@ -14,7 +14,7 @@ fi
 
 mkdir -p $DIRECTORY               # Create target directory if it doesn't exists
 
-tshark -F pcapng -b interval:15 \
+tshark -F pcapng -b interval:"$ROTATE_SECONDS" \
 -i "$INTERFACE" -f "not port 22" \
 -i lo -f "not port 22" \
--w "$DIRECTORY/$PREFIX%Y-%m-%d_%H.%M.%S.pcap"
+-w "$DIRECTORY/$PREFIX.pcap"
