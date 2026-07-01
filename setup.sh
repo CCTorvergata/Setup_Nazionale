ea06a5dd875fe532b819f19631347e20ab6d56e2
morels <clod.is.here@gmail.com>
2026-07-01 19:52:10 +0200

    Added colors to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+NC='\033[0m'
 
 grep -q "#VULNBOX_IP" inventory && { \
-	echo "ERROR: You forgot to set the vulnbox's IP in the file './inventory'"; \
+	echo -e "${RED}ERROR:${NC} You forgot to set the vulnbox's IP in the file './inventory'"; \
 	exit 1; \
 }
 
 ls ./roles/vulnbox/copy-ssh-public-keys/files/*.pub 2>&1 > /dev/null || { \
-	echo "ERROR: you need to put your SSH public key in the directory './roles/vulnbox/copy-ssh-public-keys/files'"; \
+	echo -e "${RED}ERROR:${NC} you need to put your SSH public key in the directory './roles/vulnbox/copy-ssh-public-keys/files'"; \
 	exit 1; \
 }
 
-echo "Installing ansible requirements..."
+echo
+echo -e "${GREEN}- - - Installing ansible requirements - - -${NC}"
 echo
 
 ansible-galaxy install -r requirements.yml
 
 echo
-echo "Running playbook..."
+echo -e "${GREEN}- - - Running playbook - - -${NC}"
 echo
-echo "It will ask you vulnbox's SSH password"
-echo "And the local sudo password"
+echo -e "You need to enter the ${ORANGE}vulnbox's SSH password${NC},"
+echo -e "and the ${ORANGE}local sudo password${NC}."
 echo
 
 ansible-playbook site.yml
